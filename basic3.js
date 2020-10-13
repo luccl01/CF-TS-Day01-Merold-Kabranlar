127052178299b12bfd65014467ce65c6e17ada37
Jakob Kabranlar <cankabranlar@gmail.com>
2020-10-13 15:32:46 +0200

    finishes basic3

---
+++ basic3.js
@@ -0,0 +1,12 @@
+var names = ['jakob', 'lucas', 'mila'];
+/*for (let index in names){
+    console.log(index);
+}
+
+for (let index of names){
+    console.log(index);
+}*/
+for (var index in names) {
+    console.log(index);
+    console.log(names[index]);
+}
---
+++ basic3.ts
@@ -0,0 +1,14 @@
+var names : Array<string>=['jakob','lucas','mila'];
+
+/*for (let index in names){
+	console.log(index);
+}
+
+for (let index of names){
+	console.log(index);
+}*/
+
+for (let index in names){
+	console.log(index);
+	console.log(names[index]);
+}
