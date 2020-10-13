8a770e24a50a3a685cf7f3ae90a75c5cedea101f
Lucas Merold <Lucas.merold@yahoo.de>
2020-10-13 14:48:53 +0200

    files

---
+++ test12.js
@@ -0,0 +1,15 @@
+"use strict";
+exports.__esModule = true;
+var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// array.forEach(function(x){
+//     for (let i=1; i<11;i++){
+//     console.log(x +"X"+i+"="+i*x)
+// }
+// })
+var names = [{
+        name: "Jakob",
+        lastname: "Kabranlar"
+    }];
+for (var i = 1; i < 11; i++) {
+    console.log(names[i].name);
+}
